39d65ce59a629d6ee662f758d9cd968c6e23e83b
Erica Hoi <ericahoi040506@gmail.com>
2024-08-03 12:05:52 +0800

    add responsive nav template with css

--- src/app/Kw/navbar.tsx
+++ src/app/Kw/navbar.tsx
@@ -1,9 +1,37 @@
+import "../Kw/navbat.css";
 function Navbar() {
   return (
-    <div>
-      <p>Nav here</p>
-    </div>
+    <>
+      <nav className="navbar">
+        <div className="navbar-left">
+          <a href="/" className="logo">
+            Vote
+          </a>
+        </div>
+        <div className="navbar-center">
+          <ul className="nav-links">
+            <li>
+              <a href="/form">Form</a>
+            </li>
+            <li>
+              <a href="/about">About Us</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-right">
+          <a href="/cart" className="cart-icon">
+            <i className="fas fa-shopping-cart"></i>
+            <span className="cart-count">0</span>
+          </a>
+          <a href="/account" className="user-icon">
+            <i className="fas fa-user"></i>
+          </a>
+        </div>
+      </nav>
+    </>
   );
 }
-
 export default Navbar;
