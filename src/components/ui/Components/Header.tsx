f09e151c0e75e082158d647b036fd1116076b969
Aiyern30 <ianbian2@gmail.com>
2024-08-03 15:48:19 +0800

    sketch UI

--- src/components/ui/Components/Header.tsx
+++ src/components/ui/Components/Header.tsx
@@ -1,8 +1,21 @@
+"use client";
+
 import React from "react";
 import { Button } from "../Button";
 import { Avatar, AvatarFallback, AvatarImage } from "../Avatar";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
+  const links = [
+    { name: "Home", href: "/" },
+    { name: "Overview", href: "/Overview" },
+    { name: "How it works", href: "/How-it-works" },
+    { name: "Features", href: "/Features" },
+    { name: "About Us", href: "/About-us" },
+  ];
+
   return (
     <div className="flex justify-between items-center p-4 shadow-md bg-[#DBB5B5]">
       <div className="flex items-center space-x-4">
@@ -14,22 +27,18 @@ const Header = () => {
         </div>
         <div>Home Page</div>
       </div>
-      <div className="flex items-center space-x-4">
-        <a href="#home" className="text-black">
-          Home
-        </a>
-        <a href="#overview" className="text-black">
-          Overview
-        </a>
-        <a href="#how-it-works" className="text-black">
-          How it works
-        </a>
-        <a href="#features" className="text-black">
-          Features
-        </a>
-        <a href="#about-us" className="text-black">
-          About Us
-        </a>
+      <div className="flex items-center space-x-4 cursor-pointer">
+        {links.map((link) => (
+          <a
+            key={link.name}
+            href={link.href}
+            className={`text-black ${
+              pathname === link.href ? "font-bold underline" : ""
+            }`}
+          >
+            {link.name}
+          </a>
+        ))}
         <Button className="bg-[#C39898] border border-[#2A2929] text-black">
           Login / Register
         </Button>
